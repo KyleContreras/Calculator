d96b4f3a263a2e59dd805790cb7e58292d280535
Kyle Contreras <kylecontreras@outlook.com>
2019-04-19 10:59:54 -0400

    can now push inputs to expression history using nestedArrayCheck, nstdArrayRef, and addHstryAndOop

--- public/calc.js
+++ public/calc.js
@@ -1,198 +1,279 @@
+"use strict";
+
 var calcObj = {
-    // NOTE: All instances of oop is short for 'Order of Operations'
-    
+    // Should I move these into history{}?
     historyCount: 0,
     expressionCount: 0,
-    currentExpression: [],
-    currentSolverAns: 0,
+    
+    parenthesesCount: 0,
+    closedPrnthCnt: 0,
+    
+    makeNextNumNeg: 0,
 
-    // TODO: Is an array that contains an array after condensing. Try and refactor to have a single array.
-    // will the flat() method work?
-    // OOP: Parentheses > exponents > (divide = multiply) > (add = subtract)
-    currentOop: [[],[]],
+    currentExpression: [],   
+    currentOop: [[],[],[]],
 
     history: {},
-    
-    checkHistory: function(input) {
-        if(this.historyCount === 0) {
-            // I do not think this should be here
-            // this.addToCalcDisplay(input);
-            
-            this.makeHistory();
-            this.pushOopExp(input);
+
+    calcDisplay: (input) => {
+        let elmnt = document.getElementById('display');
+        elmnt.value += input + ' ';
+
+        //elem = document.getElementById('display');
+        //elem.value = '';
+    },
+
+    checkIfNeg: function(input) {
+        if (this.makeNextNumNeg === 0) {
+            this.checkHistory(input);
         } else {
-            this.pushOopExp(input);
+            this.nextNumNeg = 0;
+            this.checkHistory((Math.abs(input) * -1));
         }
     },
 
-    // Where should this be called?
-    addToCalcDisplay: function(input) {
-        elmnt = document.getElementById('display');
-        elmnt.value += input + ' ';
+    makeNeg: function () {
+        this.makeNextNumNeg = -1;
     },
-    
-    makeHistory: function() {
-        this.historyCount++
-        Object.defineProperty(this.history, 'expression' + this.historyCount, {
-            value: {},
+
+    checkHistory: function(input) {
+        if(this.historyCount === 0) {
+            this.historyCount++;
+            this.makeExpHistory(this.history, 'expression ' + this.historyCount, {})
+        }
+
+        this.addHstryAndOop(input);
+
+        /*
+        if(isNaN(input) && input !== ')') {
+            this.addToCurrentOop(input)
+        }
+        */
+    },
+
+    makeExpHistory: function (objName, propName, value) {
+        Object.defineProperty(objName, propName, {
+            value: value,
             configurable: true,
             enumerable: true,
             writable : true,
         });
     },
 
-    pushOopExp: function(input) {
-        if (isNaN(input)) {
-            this.addToCurrentOop(input);
+    // TODO
+    nestedArrayCheck: function (crntExpProp, nstdArray, input) {
+        let expProp = crntExpProp;
+        let isThisNstdArray = nstdArray;
+        let crntInput = input === '(' ? [] : input;
+        let clsdPrnthCnt = this.closedPrnthCnt;
+        let newNstdArray;
+
+        if (Array.isArray(isThisNstdArray[isThisNstdArray.length - 1])) {
+            newNstdArray = isThisNstdArray[isThisNstdArray.length - 1];
+    
+            this.nestedArrayCheck(expProp, newNstdArray, crntInput);
+        } else if (Array.isArray(isThisNstdArray) && clsdPrnthCnt > 1) {
+            if (newNstdArray = this.nstdArrayRef(expProp, clsdPrnthCnt)) {
+                newNstdArray.push(crntInput);
+            } else {
+                isThisNstdArray.push(crntInput);
+            }
+            //newNstdArray.push(crntInput);
+            //isThisNstdArray.push(crntInput);
+        } else {
+            expProp.push(crntInput);
         }
-
-        this.addExpProp(input);
-
-        this.currentExpression.push(input)
     },
 
-    addToCurrentOop: function(input) {
-        let crntOop = this.currentOop;
-        let crntExp = this.currentExpression;
-        // had to use this. Let's see if this works.
-        let crntExpLength = crntExp.length;
-        // Is undefined. will point to an item that does not exist, if it is the first item entered.
-        let dupOpCheck = crntExpLength;
+    nstdArrayRef: function (expProp, parenthesesCount) {
+        let cpyExpProp = expProp;
+        let nstdArrayIndex;
+        let clsdPrnth = parenthesesCount;
 
-        // Can I refactor this?
-        // Will test for duplicate consecutive operators here since I will know which nested array...
-        // ... the operator will be placed in.
-        if (input === '*' && input !== dupOpCheck) {
-            crntOop[0].push(input);
-            this.addToCalcDisplay(input)
-        } else if (input === '/' && input !== dupOpCheck) {
-            crntOop[0].push(input);
-            this.addToCalcDisplay(input)
-        } else if (input !== dupOpCheckTwo) {
-            crntOop[1].push(input);
-            this.addToCalcDisplay(input)
+        if (this.parenthesesCount > clsdPrnth && clsdPrnth > 1) {
+            nstdArrayIndex = cpyExpProp[cpyExpProp.length - 1];
+            clsdPrnth--
+
+            this.nstdArrayRef(nstdArrayIndex, clsdPrnth);
         }
+
+        return nstdArrayIndex;
+    },
+
+    // Goal of fx: place input in correct spot of history object
+    // Watch out for referencing an undefined property and testing with .isArray()
+    addHstryAndOop: function(input) {
+        let expCnt = this.expressionCount;
+        let crntHstryExp = this.history['expression ' + (this.historyCount)];
+        let expProp = crntHstryExp[expCnt];
+
+        // How do I simplify this and '|| Array.isArray(expProp)'
+        if (input === '(') {
+            this.parenthesesCount++;
+            this.closedPrnthCnt++;
+
+            if(Array.isArray(expProp)) {
+                let isThisNstdArray;
+
+                // I don't think this is quite right. Can never reference the 0 index
+                // Should this if check against 0 instead of 1?
+                // Shouldn't parenthesesCount be > 1 as well?
+                if (expProp.length - 1 > 1) {
+                    isThisNstdArray = expProp[(expProp.length - 1)];
+                } else {
+                    isThisNstdArray = expProp;
+                }
+                
+                this.nestedArrayCheck(expProp, isThisNstdArray, input);
+            } else {
+                this.makeExpHistory(crntHstryExp, expCnt, [])
+            }
+        } else if (input === ')') {
+            this.closedPrnthCnt--;
+
+            if(this.closedPrnthCnt === 0) {
+                this.expressionCount++;
+            }
+        } else if (Array.isArray(expProp)) {
+            let isThisNstdArray;
+
+            if (expProp.length - 1 > 1) {
+                isThisNstdArray = expProp[(expProp.length - 1)];
+            } else {
+                isThisNstdArray = expProp;
+            }
+            
+            this.nestedArrayCheck(expProp, isThisNstdArray, input);
+        } else {
+            this.makeExpHistory(crntHstryExp, expCnt, input);
+            this.expressionCount++;
+        }
+        
+        this.calcDisplay(input);
+        // Test
+        console.log(this.history);
+    },
+
+    cnvrtHstryToArrayExp: function(exp) {
+        this.currentExpression = Object.values(exp);
     },
     
-    // Test logs here
-    addExpProp: function(input) {
-        this.history['expression' + (this.historyCount)][this.expressionCount] = input;
-        this.expressionCount++;
+    // TODO
+    addToCurrentOop: function(input) {
+        let crntOop = this.currentOop;
+        /*
+        let expCnt = this.expressionCount;
+        let crntHstryExp = this.history['expression ' + (this.historyCount)];
+        let expProp;
 
-        this.addToCalcDisplay(input);
-        
-        // Tests
-        console.log(this.history);
-        console.log(this.currentExpression);
+        // Ugly solution
+        if (expCnt === 0) {
+            expProp = crntHstryExp[expCnt];
+        } else {
+            expProp = crntHstryExp[expCnt - 1];
+        }
+        */
+
+        // dupOpCheck = crntExp.length -- Is it necessary now?
+        if (Array.isArray(input)) {
+            crntOop[0].push([]);
+        } else if ((input === '*' || input === '/') /*&& (input !== dupOpCheck)*/) {
+            crntOop[1].push(input);
+        } else if ((input === '+' || input === '-') /*&& (input !== dupOpCheck)*/) {
+            crntOop[2].push(input);
+        }
+
+        //Test
         console.log(this.currentOop);
     },
     
-    // may need to revisit
-    concatOopArrays: function() {
-        var crntOop = this.currentOop;       
-        var cmbndArrays = crntOop[0].concat(crntOop[1]);
+    concatOopArrays: function () {
+        let crntOop = this.currentOop;
+        let OopReplacement = [];
 
-        crntOop.push(cmbndArrays);
-
-        this.OopCleanUp(crntOop);
+        for (let i = 0; i < crntOop.length; i++) {
+            crntOop[i].forEach((element) => {
+                OopReplacement.push(element);
+            })
+        }
+        this.currentOop = OopReplacement;
+        console.log(this.currentOop);
     },
 
+    // Splice second argument not dynamic to the situation.
     OopCleanUp: function() {
-        var operatorArray = this.currentOop;
+        let operatorArray = this.currentOop;
         operatorArray.splice(0,2);
     },
 
-    operatorCheck: function() {
-        var crntOop = this.currentOop[0];
-        var crntExp = this.currentExpression;
-        var i, j;
+    // TODO
+    operatorCompare: function() {
+        let crntOop = this.currentOop;
+        let crntExp = this.currentExpression;
 
-        for (i = 0; i < crntOop.length; i++) {
-            for (j = 0; j < crntExp.length; j++) {   
-                // NOTE: crntOop is an array that contains an array. Not as intended.
+        for (let i = 0; i < crntOop.length; i++) {
+            for (let j = 0; j < crntExp.length; j++) {   
                 if(crntOop[i] === crntExp[j]) {
-                    var mathOprtr = crntOop[i];
+                    var oprtr = crntOop[i];
                     var numA = crntExp[j-1];
                     var numB = crntExp[j+1];
                     var spliceStart = (j - 1);
+                    let crntSolverAns = this.solveExpression(oprtr, numA, numB);
 
-                    // NOTE: this var probably hold up for equations with () or exponents. Works for now.
-                    //var crntExpOprtr = crntExp[j-1];
-                    
-                    this.solver(mathOprtr, numA, numB);
-
-                    var cSA = this.currentSolverAns;
-                    // This returns an array of the deleted items. Not sure how long said arrays hang around for.
-                    // Not the cleanest solution but it will do for now...
-                    this.operatorReplacer(spliceStart, 3, cSA)
+                    // The middle argument might end up being a problem because it is not dynamic.
+                    this.operatorReplacer(spliceStart, 3, crntSolverAns)
 
                     break;
                 }
             }
         }
-        this.addToCalcDisplay(this.currentExpression);
+        // Should I add the '=' here?
+        // crntExp
+        this.calcDisplay(crntExp);
     },
 
-    solver: function(symbol, nmbrA, nmbrB) {
-        switch(symbol) {
+    solveExpression: function(operator, numA, numB) {
+        switch(operator) {
             case '*':
-                this.multiply(nmbrA, nmbrB);
-                break;
+                return numA * numB;
             case '/':
-                this.divide(nmbrA, nmbrB);
-                break;
+                return numA / numB;
             case '+':
-                this.add(nmbrA, nmbrB);
-                break;
+                return numA + numB;
             case '-':
-                this.subtract(nmbrA, nmbrB);
-                break;
+                return numA - numB;
         }
     },
     
-    // NOTE: this could be streamlined?
-    operatorReplacer: function(expStart, delCnt, addAns) {
+    // can this be streamlined?
+    operatorReplacer: function(expStart, delCnt, solverAns) {
         var crntExp = this.currentExpression;
 
-        crntExp.splice(expStart, delCnt, addAns)
+        crntExp.splice(expStart, delCnt, solverAns)
     },
 
-    add: function(a, b) {
-        var ans = a + b;
-        this.currentSolverAns = ans;
-    },
-    
-    subtract: function(a, b) {
-        var ans = a - b;
-        this.currentSolverAns = ans;
-    },
-    
-    multiply: function(a, b) {
-        var ans = a * b;
-        this.currentSolverAns = ans;
-    },
-    
-    divide: function(a, b) {
-        var ans = a / b;
-        this.currentSolverAns = ans;
+    // I put makeHistory() here for now?
+    resetCounters: function () {
+        this.expressionCount = 0;
+        this.currentExpression = [];
+        this.currentOop = [[],[],[]];
+
+        //this.makeHistory();
     },
 
     // TODO
     showHistory: function() {},
     
-    addEqualSign: function() {
-        this.addToCalcDisplay('=');
-        this.addExpProp('='); 
-    },
-    
-    // TODO: Don't forget to reset currentSolverAns, currentExpression, and currentOop
-    // Don't forget to uncomment makeHistory()
+    // Should this only call cnvrtHstryToArrayExp()?
     findAnswer: function() {
-        this.addEqualSign();
+        let crntExp = this.history['expression ' + this.historyCount];
+        
+        this.cnvrtHstryToArrayExp(crntExp);
+
+        this.addToCurrentOop();
         this.concatOopArrays();
-        this.operatorCheck();
-        this.expressionCount = 0;
-        //this.makeHistory();
+        this.operatorCompare();
+        this.resetCounters();
     },
     
     // TODO: Make sure everything is reset accordingly.
@@ -200,10 +281,6 @@ var calcObj = {
         elem = document.getElementById('display');
         elem.value = '';
         
-        this.expressionCount = 0;
-        this.currentSolverAns = 0;
-        this.currentExpression = [];
-        this.currentOop = [[],[]]
-        this.makeHistory();
-    }, 
-}
+        this.resetCounters();
+    } 
+};
